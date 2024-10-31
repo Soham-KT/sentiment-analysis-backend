2cc3394ed88c5cb4877d395628fb289637ce1b81
Soham-KT <sohamkothariprof79@gmail.com>
2024-10-31 15:53:04 +0530

    changes

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
-from http.server import BaseHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
 
-class handler(BaseHTTPRequestHandler):
-
+class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
             self.send_response(200)
@@ -23,3 +23,12 @@ class handler(BaseHTTPRequestHandler):
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b'404 Not Found')
+
+def run(server_class=HTTPServer, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, Handler)
+    print(f'Starting server on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
