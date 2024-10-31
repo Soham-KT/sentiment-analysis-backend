2de6b041f28efd53fed650d32c63e726f3bef22b
Soham-KT <sohamkothariprof79@gmail.com>
2024-10-31 15:58:19 +0530

    addition of files

--- api/index.py
+++ api/index.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
+import urllib.parse
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -11,4 +12,22 @@ class Handler(BaseHTTPRequestHandler):
             'Review': "This DVD will be a disappointment if you get it hoping to see some substantial portion of the acts of the various comics listed on the cover. All you get here are snippets of performance, at best. The rest is just loose-leaf reminiscence about the good old days in Boston, in the early 80's, when a lot of comics were hanging out together and getting their start. It's like a frat house reunion. There's a lot of lame nostalgia. There are quite a few guffaws recalling jokes (practical and otherwise) perpetrated - back then. But you had to have been there to appreciate all the basically good ol' boy camaraderie. If you weren't actually a part of that scene, all this joshing and jostling will fall flat. If you want to actually hear some of these comics' routines - you will have to look elsewhere.",
             'prediction': 'negative',
         }
+        self.wfile.write(json.dumps(data).encode('utf-8'))
+        
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        parsed_data = urllib.parse.parse_qs(post_data.decode('utf-8'))
+
+        text = parsed_data.get('review', [None])[0]
+        prediction = model_predict(text=text)  # Ensure model_predict is defined/imported
+
+        data = {
+            'review': text,
+            'prediction': prediction
+        }
+
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
         self.wfile.write(json.dumps(data).encode('utf-8'))
