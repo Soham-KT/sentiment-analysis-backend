b03e44d1ae6fadd929138c69f73da6cf8802b8b4
Soham-KT <sohamkothariprof79@gmail.com>
2024-11-02 12:32:34 +0530

    back to flask

--- api/index.py
+++ api/index.py
@@ -1,35 +1,38 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import json
-import urllib.parse
-from data_predict import model_predict
+from flask import Flask, request, jsonify
+from data_predict import model_predict  # Ensure this function is correctly imported
 
+app = Flask(__name__)
 
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        
-        data = {
-            'Review': "This DVD will be a disappointment if you get it hoping to see some substantial portion of the acts of the various comics listed on the cover. All you get here are snippets of performance, at best. The rest is just loose-leaf reminiscence about the good old days in Boston, in the early 80's, when a lot of comics were hanging out together and getting their start. It's like a frat house reunion. There's a lot of lame nostalgia. There are quite a few guffaws recalling jokes (practical and otherwise) perpetrated - back then. But you had to have been there to appreciate all the basically good ol' boy camaraderie. If you weren't actually a part of that scene, all this joshing and jostling will fall flat. If you want to actually hear some of these comics' routines - you will have to look elsewhere.",
-            'prediction': 'negative',
-        }
-        self.wfile.write(json.dumps(data).encode('utf-8'))
-        
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length)
-        parsed_data = urllib.parse.parse_qs(post_data.decode('utf-8'))
+@app.route('/')
+def hello_word():
+    return 'Hello, World!'
 
-        text = parsed_data.get('review', [None])[0]
-        prediction = model_predict(text=text)  # Ensure model_predict is defined/imported
+@app.route('/data', methods=['GET'])
+def handle_get():
+    # This is the data that will be returned for a GET request
+    data = {
+        'Review': "This DVD will be a disappointment if you get it hoping to see some substantial portion of the acts of the various comics listed on the cover. All you get here are snippets of performance, at best. The rest is just loose-leaf reminiscence about the good old days in Boston, in the early 80's, when a lot of comics were hanging out together and getting their start. It's like a frat house reunion. There's a lot of lame nostalgia. There are quite a few guffaws recalling jokes (practical and otherwise) perpetrated - back then. But you had to have been there to appreciate all the basically good ol' boy camaraderie. If you weren't actually a part of that scene, all this joshing and jostling will fall flat. If you want to actually hear some of these comics' routines - you will have to look elsewhere.",
+        'prediction': 'negative',
+    }
+    return jsonify(data), 200
 
-        data = {
-            'review': text,
-            'prediction': prediction
-        }
+@app.route('/predict', methods=['POST'])
+def handle_post():
+    data = request.get_json()  # Extract JSON data from the request
+    text = data.get('review')
 
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        self.wfile.write(json.dumps(data).encode('utf-8'))
+    if not text:
+        return jsonify({'error': 'Review text is missing'}), 400
+
+    # Make prediction using the imported model_predict function
+    prediction = model_predict(text=text)
+
+    response_data = {
+        'review': text,
+        'prediction': prediction
+    }
+
+    return jsonify(response_data), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
