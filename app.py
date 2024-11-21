fd762492fa6b6aee299755a6084bb2e5a22b9383
Soham-KT <sohamkothariprof79@gmail.com>
2024-11-22 01:05:11 +0530

    some changes

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def hello_word():
 @app.route('/data', methods=['GET'])
 def handle_get():
     data = {
-        'Review': "This DVD will be a disappointment if you get it hoping to see some substantial portion of the acts of the various comics listed on the cover. All you get here are snippets of performance, at best. The rest is just loose-leaf reminiscence about the good old days in Boston, in the early 80's, when a lot of comics were hanging out together and getting their start. It's like a frat house reunion. There's a lot of lame nostalgia. There are quite a few guffaws recalling jokes (practical and otherwise) perpetrated - back then. But you had to have been there to appreciate all the basically good ol' boy camaraderie. If you weren't actually a part of that scene, all this joshing and jostling will fall flat. If you want to actually hear some of these comics' routines - you will have to look elsewhere. This product is shit",
+        'Review': "This DVD will be a disappointment if you get it hoping to see some substantial portion of the acts of the various comics listed on the cover. All you get here are snippets of performance, at best. The rest is just loose-leaf reminiscence about the good old days in Boston, in the early 80's, when a lot of comics were hanging out together and getting their start. It's like a frat house reunion. There's a lot of lame nostalgia. There are quite a few guffaws recalling jokes (practical and otherwise) perpetrated - back then. But you had to have been there to appreciate all the basically good ol' boy camaraderie. If you weren't actually a part of that scene, all this joshing and jostling will fall flat. If you want to actually hear some of these comics' routines - you will have to look elsewhere.",
         'prediction': 'negative',
     }
     return jsonify(data)
@@ -31,4 +31,4 @@ def handle_post():
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
--- data_predict.py
+++ data_predict.py
@@ -43,10 +43,10 @@ def model_predict(text, tokenizer=lstm_tokenizer, model=lstm_model):
     sequence = tokenizer.texts_to_sequences([clean_text])
     padded_sequence = pad_sequences(sequence, maxlen=100)
     prediction = model.predict(padded_sequence)
-    
-    if prediction < 0.4:
+        
+    if prediction < 0.6:
         pred = 'Negative'
-    elif  prediction < 0.6 and prediction >= 0.4:
+    elif  prediction == 0.6:
         pred = 'Neutral'
     else:
         pred = 'Positive'
@@ -55,7 +55,9 @@ def model_predict(text, tokenizer=lstm_tokenizer, model=lstm_model):
 
 
 if __name__ == '__main__':
-    text = "This DVD will be a disappointment if you get it hoping to see some substantial portion of the acts of the various comics listed on the cover. All you get here are snippets of performance, at best. The rest is just loose-leaf reminiscence about the good old days in Boston, in the early 80's, when a lot of comics were hanging out together and getting their start.It's like a frat house reunion. There's a lot of lame nostalgia. There are quite a few guffaws recalling jokes (practical and otherwise)perpetrated - back then. But you had to have been there to appreciate all the basically good ol' boy camaraderie. If you weren't actually a part of that scene, all this joshing and jostling will fall flat.If you want to actually hear some of these comics' routines - you will have to look elsewhere."
+    # text = "This DVD will be a disappointment if you get it hoping to see some substantial portion of the acts of the various comics listed on the cover. All you get here are snippets of performance, at best. The rest is just loose-leaf reminiscence about the good old days in Boston, in the early 80's, when a lot of comics were hanging out together and getting their start.It's like a frat house reunion. There's a lot of lame nostalgia. There are quite a few guffaws recalling jokes (practical and otherwise)perpetrated - back then. But you had to have been there to appreciate all the basically good ol' boy camaraderie. If you weren't actually a part of that scene, all this joshing and jostling will fall flat.If you want to actually hear some of these comics' routines - you will have to look elsewhere."
+    
+    text = 'This was a bad day'
     
     result = model_predict(text)
     
